a65b5e356f38ed73033bab9b8a2e29523a12b1cd
Tatiana Isaeva <61195639+IsaevaTatyana@users.noreply.github.com>
2021-03-10 11:35:08 +0400

    Add files via upload

---
+++ Task_1/Task_1_2/task_1_2_4/task_1_2_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task_1_2_4
+{
+    class MainClass
+    {
+        //task_1_2_4
+            static void splitString(string enter)
+            {
+                char[] text = enter.ToCharArray();
+                bool toUp = true;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (char.IsLetter(text[i]))
+                    {
+                        if (toUp == true)
+                        {
+                            text[i] = char.ToUpper(text[i]);
+                            toUp = false;
+                        }
+                    }
+                    if (text[i] == '!' || text[i] == '?' || text[i] == '.')
+                    {
+                        toUp = true;
+                    }
+                }
+
+                foreach (char i in text)
+                {
+                    Console.Write(i);
+                }
+            }
+        
+
+        static void Main(string[] args)
+        {
+                string myStr = "я плохо учил русский язык. забываю начинать предложения с заглавной. хорошо, что можно написать программу!";
+                Console.WriteLine(myStr);
+                splitString(myStr);
+        }
+    }
+}
